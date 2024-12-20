73b136ea44ffee1cae29f196091ad8f99e57f8e1
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-12-20 14:56:33 +0300

    workschedule controller created

---
+++ src/Presentation/Hsm.Api/Controllers/WorkScheduleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hsm.Api.Controllers
+{
+    public class WorkScheduleController : BaseController
+    {
+
+        [HttpPost]
+        [Route("create-workschedule")]
+        public async Task<IActionResult> CreateWorkSchedule()
+        {
+            return Ok();
+        }
+    }
+}
