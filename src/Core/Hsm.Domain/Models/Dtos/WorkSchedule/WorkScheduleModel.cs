c86223901581a1bbf908c4da2959e916cc833502
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-12-21 12:10:21 +0300

    workSchedule model added

---
+++ src/Core/Hsm.Domain/Models/Dtos/WorkSchedule/WorkScheduleModel.cs
@@ -0,0 +1,22 @@
+﻿using Hsm.Domain.Models.Dtos.Appointment;
+using Hsm.Domain.Models.Dtos.Doctor;
+using ModelMapper;
+
+namespace Hsm.Domain.Models.Dtos.WorkSchedule
+{
+    public class WorkScheduleModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte[] RowVersion { get; set; } = null!;
+        public bool IsActive { get; set; }
+
+        [PropertyMapping("DoctorModel")]
+        public DoctorModel DoctorModel { get; set; }
+
+        [PropertyMapping("AppointmentModel")]
+        public List<AppointmentModel> AppointmentModels { get; set; }
+    }
+}
