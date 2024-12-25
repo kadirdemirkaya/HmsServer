50569362ba81f5edbdd70a565c0b08dd2e8702e6
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-12-25 16:17:37 +0300

    unimportant  changes

--- src/Infrastructure/Hsm.Persistence/Context/HsmSeedDb.cs
+++ src/Infrastructure/Hsm.Persistence/Context/HsmSeedDb.cs
@@ -113,6 +113,54 @@ namespace Hsm.Persistence.Context
                         TcNumber = "56776556776"
                     }
                 };
+                defaultUsers.Add(new()
+                {
+                    Id = Guid.Parse("C04E99E0-D639-48CC-9B21-4B0105342370"),
+                    UserName = "MehmetYılmaz",
+                    Email = "MehmetYılmaz@example.com",
+                    EmailConfirmed = true,
+                    TcNumber = "12340008900"
+                });
+                defaultUsers.Add(new()
+                {
+                    Id = Guid.Parse("ECFF321E-FDDB-4C7C-A7D1-4BE0985FECC9"),
+                    UserName = "SelinArslan",
+                    Email = "SelinArslan@example.com",
+                    EmailConfirmed = true,
+                    TcNumber = "11140008900"
+                });
+                defaultUsers.Add(new()
+                {
+                    Id = Guid.Parse("4CE15744-9ECE-4D79-856B-E0CE9BC7C797"),
+                    UserName = "SelinArslan",
+                    Email = "SelinArslan@example.com",
+                    EmailConfirmed = true,
+                    TcNumber = "11140008900"
+                });
+                defaultUsers.Add(new()
+                {
+                    Id = Guid.Parse("8A50CD69-71C1-4DFB-AA0B-B08EBA654C95"),
+                    UserName = "AliDemir",
+                    Email = "AliDemir@example.com",
+                    EmailConfirmed = true,
+                    TcNumber = "11140008922"
+                });
+                defaultUsers.Add(new()
+                {
+                    Id = Guid.Parse("05B126AF-4B08-4930-B244-2E2121318273"),
+                    UserName = "AyhanKara",
+                    Email = "AyhanKara@example.com",
+                    EmailConfirmed = true,
+                    TcNumber = "13340008922"
+                });
+                defaultUsers.Add(new()
+                {
+                    Id = Guid.Parse("F1A8E205-C476-495C-ABB3-B4F0E175E833"),
+                    UserName = "ZeynepÇelik",
+                    Email = "ZeynepÇelik@example.com",
+                    EmailConfirmed = true,
+                    TcNumber = "14440008922"
+                });
 
                 foreach (var defaultUser in defaultUsers)
                 {
@@ -193,11 +241,53 @@ namespace Hsm.Persistence.Context
                 hospital4.AddDoctorToHospital(doctor4_2);
                 #endregion
 
+                #region 5. Hastane
+                Hospital hospital5 = Hospital.Create(
+                    "İstanbul Medipol Mega Hastanesi",
+                    Address.Create("Bağcılar Caddesi", "İstanbul", "active", "34200", "Türkiye", "Bağcılar"),
+                    "+90 212 460 7777",
+                    City.Create("İstanbul")
+                );
+
+                Clinical cardiology5 = Clinical.Create("Kardiyoloji", hospital5.Id);
+                hospital5.AddClinicalToHospital(cardiology5);
+
+                Doctor doctor5_1 = Doctor.Create("Dr. Mehmet", "Yılmaz", "Kardiyolog", "Schedule", Guid.Parse("C04E99E0-D639-48CC-9B21-4B0105342370"));
+                Doctor doctor5_2 = Doctor.Create("Dr. Selin", "Arslan", "Kardiyolog", "Schedule", Guid.Parse("ECFF321E-FDDB-4C7C-A7D1-4BE0985FECC9"));
+                Doctor doctor5_3 = Doctor.Create("Dr. Ali", "Demir", "Kardiyolog", "Schedule", Guid.Parse("8A50CD69-71C1-4DFB-AA0B-B08EBA654C95"));
+
+                hospital5.AddDoctorToHospital(doctor5_1);
+                hospital5.AddDoctorToHospital(doctor5_2);
+                hospital5.AddDoctorToHospital(doctor5_3);
+                #endregion
+
+                #region 6. Hastane 
+                Hospital hospital6 = Hospital.Create(
+                    "Ankara Şehir Hastanesi",
+                    Address.Create("Kızılırmak Mahallesi", "Ankara", "active", "06800", "Türkiye", "Çankaya"),
+                    "+90 312 552 6000",
+                    City.Create("Ankara")
+                );
+
+                Clinical oncology6 = Clinical.Create("Onkoloji", hospital6.Id);
+                hospital6.AddClinicalToHospital(oncology6);
+
+                Doctor doctor6_1 = Doctor.Create("Dr. Ayhan", "Kara", "Onkolog", "Schedule", Guid.Parse("05B126AF-4B08-4930-B244-2E2121318273");
+                Doctor doctor6_2 = Doctor.Create("Dr. Zeynep", "Çelik", "Onkolog", "Schedule", Guid.Parse("F1A8E205-C476-495C-ABB3-B4F0E175E833"));
+
+                hospital6.AddDoctorToHospital(doctor6_1);
+                hospital6.AddDoctorToHospital(doctor6_2);
+                #endregion
+
+
+
 
                 await _writeHospitalRepo.AddAsync(hospital);
                 await _writeHospitalRepo.AddAsync(hospital2);
                 await _writeHospitalRepo.AddAsync(hospital3);
                 await _writeHospitalRepo.AddAsync(hospital4);
+                await _writeHospitalRepo.AddAsync(hospital5);
+                await _writeHospitalRepo.AddAsync(hospital6);
                 await _writeDoctorRepo.SaveChangesAsync();
             }
         }
