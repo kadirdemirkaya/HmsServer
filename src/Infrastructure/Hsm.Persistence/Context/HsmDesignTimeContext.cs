469e43fc8a84068b91ae7e4c38dfb6d0d58ec156
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-12-25 19:48:24 +0300

    url and compose file updated

--- src/Infrastructure/Hsm.Persistence/Context/HsmDesignTimeContext.cs
+++ src/Infrastructure/Hsm.Persistence/Context/HsmDesignTimeContext.cs
@@ -10,10 +10,13 @@ namespace Hsm.Persistence.Context
     {
         public HsmDbContext CreateDbContext(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
             var configuration = new ConfigurationBuilder()
-               .SetBasePath($"{Directory.GetCurrentDirectory()}{"/../../Presentation/Hsm.Api"}")
-               .AddJsonFile("appsettings.json")
-               .Build();
+                .SetBasePath($"{Directory.GetCurrentDirectory()}{"/../../Presentation/Hsm.Api"}")
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .Build();
 
             DbContextOptionsBuilder<HsmDbContext> dbContextOptionsBuilder = new();
 
