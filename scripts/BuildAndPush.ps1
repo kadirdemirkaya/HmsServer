6c75cbe7a839587ff95a6a9ad1f7d7989a8bb000
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2025-01-03 14:34:03 +0300

    ed

---
+++ scripts/BuildAndPush.ps1
@@ -0,0 +1,14 @@
+$imageName = "kadirkdr/hsmserverapi" 
+$tag = "v22" 
+$dockerfilePath = "src\Presentation\Hsm.Api\Dockerfile"
+
+# Docker build işlemi
+Write-Host "Docker image creating..."
+docker build -t "${imageName}:${tag}" -f $dockerfilePath .
+
+# Docker Hub'a push işlemi
+Write-Host "Dockerhub push in the progress ..."
+docker login
+docker push "${imageName}:${tag}"
+
+Write-Host "Docker image successfully built and pushed to DockerHub."
